2bbbab3235fe5a1878e1efdc2f0dfa721bbbb7b9
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-02-14 19:29:45 +0100

    Add project files.

---
+++ Familjeträd/Databas.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Familjeträd
+{
+    class Databas
+    {
+        public string ConnectionString { get; set; } = "Data Source=DESKTOP-NJ9EFR0; Integrated Security = True;database={0}";
+        public string DatabaseName { get; set; } = "Familjeträd";
+        public string Master { get; set; } = "master";
+        internal long SQL(string sqlString, params (string, string)[] parameters)
+        {
+            long rows = 0;
+            try
+            {
+                var conn = string.Format(ConnectionString, DatabaseName);
+                using (var cnn = new SqlConnection(conn))
+                {
+                    cnn.Open();
+                    using (var command = new SqlCommand(sqlString, cnn))
+                    {
+                        Parameters(parameters, command);
+                        rows = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            return rows;
+        }
+
+        internal DataTable GetDataTable(string sqlString, string databaseName, params (string, string)[] parameters)
+        {
+            var dt = new DataTable();
+            var connString = string.Format(ConnectionString, databaseName);
+            try
+            {
+                using (var cnn = new SqlConnection(connString))
+                {
+                    cnn.Open();
+                    using (var command = new SqlCommand(sqlString, cnn))
+                    {
+                        Parameters(parameters, command);
+
+                        try
+                        {
+                            using (var adapter = new SqlDataAdapter(command))
+                            {
+                                adapter.Fill(dt);
+                            }
+                        }
+                        catch (System.Exception ex)
+                        {
+                            Debug.WriteLine(ex);
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return dt;
+        }
+
+        private static void Parameters((string, string)[] parameters, SqlCommand command)
+        {
+            foreach (var item in parameters)
+            {
+                command.Parameters.AddWithValue(item.Item1, item.Item2);
+            }
+        }
+        internal bool FinnsDatabas(string name)
+        {
+            var databas = GetDataTable("SELECT name FROM sys.databases WHERE name = @name", Master, ("@name", name));
+            return databas.Rows.Count > 0;
+        }
+
+        internal void CreateDatabase(string databaseName)
+        {
+            SQL($"CREATE DATABASE {databaseName}");
+        }
+
+        internal void CreateDatatable(string datatableName, string fields)
+        {
+            SQL($"CREATE TABLE {datatableName} ({fields});");
+        }
+    }
+}
